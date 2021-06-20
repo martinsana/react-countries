5e3e455af31813a5f2525206623b582ba728d781
Ana Paula <anapaula.martins17@hotmail.com>
2021-06-20 19:24:28 -0300

    React countries page

---
+++ src/components/TextInput.js
@@ -0,0 +1,29 @@
+export default function TextInput({
+  labelDescription = 'Descrição do campo: ',
+  inputValue = 'Descrição do campo:',
+  onInputChange = null,
+  id = 'id_input_text',
+  autoFocus,
+}) {
+  function handleInputChange({ currentTarget }) {
+    if (onInputChange) {
+      const newValue = currentTarget.value;
+      onInputChange(newValue);
+    }
+  }
+  return (
+    <div className="flex flex-col my-4">
+      <label className="mb-1" htmlFor={id}>
+        {labelDescription}
+      </label>
+      <input
+        autoFocus={autoFocus}
+        className="p-1 border "
+        id={id}
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+}
---
+++ src/pages/ReactCountriesPage.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+
+export default function ReactCountriesPage() {
+  return (
+    <>
+      <Header size="large">React Countries</Header>
+
+      <Main>Conteúdo da página</Main>
+    </>
+  );
+}
